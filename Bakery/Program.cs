1da2efdbc34e0450bdfa5c39dfa01efb0c5e7c07
Ben Russell <benjaminrussell36@gmail.com>
2020-07-17 16:14:04 -0700

    Refactor Main method to make void and just Console.WriteLine to user.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,28 +6,24 @@ namespace Pierres
 {
   public class Bakery
   {
-    public static int Main()
+    public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery! We have Bread and Pastries.");
       Console.WriteLine("Please enter (Bread/Pastry) to specify how many you would like of each.");
       string userOrder = Console.ReadLine();
-      int breadOrder;
-      int pastryOrder;
       if (userOrder == "Bread")
       {
         Console.WriteLine("How many loaves would you like? Enter a number: ");
         Bread newOrder = new Bread(int.Parse(Console.ReadLine()));
         breadOrder = newOrder.BreadValue();
-        return breadOrder;
       }
       else if (userOrder == "Pastry")
       {
         Console.WriteLine("How many pastries would you like? Enter a number: ");
         Pastry newOrder = new Pastry(int.Parse(Console.ReadLine()));
         pastryOrder = newOrder.PastryValue();
-        return pastryOrder;
       }
-      return Console.WriteLine("You owe $" + breadOrder + pastryOrder);
+      Console.WriteLine("You owe $" + breadOrder + pastryOrder);
     }
   }
 }
